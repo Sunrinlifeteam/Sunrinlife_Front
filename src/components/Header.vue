25a5896859e1f80c8c3483d49f83637578d3e4ef
ckstmznf <ckstmznf@naver.com>
2022-02-04 22:55:18 +0900

    Header 수정

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,23 @@
 <template>
 <header>
-    <div class="user-profile font-medium">
-        <!-- {{ currentUserData.profileImg }} -->
-        <img
-            :src="currentUserData.profileImg" alt="유저 사진"
-            @click="navOnOff"
-            >
-        
-        <span class="user-profile-data">{{currentUserData.year}}학년</span>
-        <span class="user-profile-data">{{currentUserData.class}}반</span>
-        <span class="user-profile-data">{{currentUserData.number}}번</span>
-        <span class="user-profile-data">{{currentUserData.name}}</span>
-    </div>
+    <div class="menu">
+        <div class="user-profile">
+            <img
+                @click="$emit('navOnOff')"
+                :src="currentUserData.profileImg" alt="사용자 프로필">
 
-    <img src="./../assets/logo.svg" alt="Logo" class="logo">
+            <span class="user-profile-data">{{ currentUserData.year }}학년</span>
+            <span class="user-profile-data">{{ currentUserData.class }}반</span>
+            <span class="user-profile-data">{{ currentUserData.number }}번</span>
+            <span class="user-profile-data">{{ currentUserData.name }}</span>
+        </div>
+        <img
+            @click="$router.push('/main')"
+            src="./../assets/logo.svg" alt="sunrin life Logo" class="logo">
+    </div>
+    <div class="school-img">
+        <p>우리들의 두번째 학교 <b>선린 Life</b></p>
+    </div>
 </header>
 </template>
 
@@ -42,13 +46,25 @@ export default {
 
 <style>
 header {
-    background-color: var(--main-color2);
     
-    padding : 16px;
+}
+
+header .menu {
+    background-color: white;
+    
+    height : 70px;
+
+    padding : 12px;
+
+    box-shadow: 0px -20px 60px var(--gray8);
 
     display: flex;
     justify-content: space-between;
-    align-items: center;
+
+    position: fixed;
+    top : 0px;
+    left : 0px;
+    right : 0px;
 }
 
 header .user-profile {
@@ -56,29 +72,36 @@ header .user-profile {
     align-items: center;
 }
 
-header .user-profile img{
-    width : 10%; height : 10%;
+header .user-profile img {
+    width : 40px;
 
     border-radius: 100%;
-
-    margin-right : 16px;
 
     cursor: pointer;
 }
 
-header .user-profile-data {
-    font-size: 1.25em;
-    margin : 0 4px;
+header .user-profile-data{
+    font-size: 22px;
+    margin : 0 8px;
 }
 
-header .logo{
-    width : 30%;
+header .logo {
+    width : 250px;
+
+    cursor: pointer;
 }
 
+header .school-img {
+    background-color: var(--flat-light-blue);
 
-@media (min-width: 600px) {
-    header .logo{
-        width : 15% 
+    height : 300px;
+
+    margin-top : 70px;
+}
+
+@media (max-width : 750px) {
+    header .menu .logo {
+        width : 20%;
     }
 }
 </style>
