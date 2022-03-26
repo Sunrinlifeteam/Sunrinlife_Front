4ca032a9b05f52537172dd771a7d068efa65fcb5
ckstmznf <ckstmznf@naver.com>
2022-03-26 12:24:09 +0900

    API 연동 코드 작성, 연동 X

--- src/api.js
+++ src/api.js
@@ -40,14 +40,33 @@ export async function editProfile(githubLink, image, description, clubInfo){
 }
 
 // 동아리 api
+
+// 전체 동아리 가져오기
 export async function getClubAll(){
     let clubData = await getAPI.get("/club/all")
         .then((res) => {
-            // console.log("가져옴");
-            // console.log(res.data);
             res.data
         })
         .catch(e => console.log(e))
+
+    return clubData
+}
+
+// 학과별 전공동아리 가져오기
+// 인자를 받아서 해당 학과의 전공 동아리를 가져옵니다.
+export async function getClubMajor(dep){
+    let clubData = await getAPI.get(`/club/department/${dep}/type/0`)
+        .then(res => res.data)
+        .catch(console.log)
+        
+    return clubData
+    }
+    
+// 일반동아리 가져오기
+export async function getClubGeneral() {
+    let clubData = await getAPI.get(`/club/type/1`)
+        .then(res => res.data)
+        .catch(console.log)
 
     return clubData
 }
