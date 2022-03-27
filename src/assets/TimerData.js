56fcfcfc21fcc9c12a5846d420cd9b88d94cc247
ckstmznf <ckstmznf@naver.com>
2022-03-27 13:54:47 +0900

    타이머 시간 데이터 변경

--- src/assets/TimerData.js
+++ src/assets/TimerData.js
@@ -17,84 +17,84 @@ export default [
     new TimerItem(
         "1교시",
         new Time(8, 40, 0),
-        new Time(9, 29, 59),
+        new Time(9, 20, 59),
         "study"
     ),
     new TimerItem(
         "1교시 쉬는시간",
         new Time(9, 30, 0),
-        new Time(9, 34, 59),
+        new Time(9, 40, 59),
         "not study"
     ),
     new TimerItem(
         "2교시",
-        new Time(9, 35, 0),
-        new Time(10, 24, 59),
+        new Time(9, 41, 0),
+        new Time(10, 9, 59),
         "study"
     ),
     new TimerItem(
         "2교시 쉬는시간",
-        new Time(10, 25, 0),
-        new Time(10, 29, 59),
+        new Time(10, 10, 0),
+        new Time(10, 19, 59),
         "not study"
     ),
     new TimerItem(
         "3교시",
-        new Time(10, 30, 0),
-        new Time(11, 24, 59),
+        new Time(10, 20, 0),
+        new Time(10, 59, 59),
         "study"
     ),
     new TimerItem(
         "점심시간",
-        new Time(11, 25, 0),
-        new Time(12, 14, 59),
+        new Time(11, 0, 0),
+        new Time(11, 59, 59),
         "launch"
     ),
     new TimerItem(
         "4교시",
-        new Time(12, 15, 0),
-        new Time(13, 4, 59),
+        new Time(12, 0, 0),
+        new Time(12, 39, 59),
         "study"
     ),
     new TimerItem(
         "4교시 쉬는시간",
-        new Time(13, 5, 0),
-        new Time(13, 9, 59),
+        new Time(12, 40, 0),
+        new Time(12, 49, 59),
         "not study"
     ),
     new TimerItem(
         "5교시",
-        new Time(13, 10, 0),
-        new Time(13, 59, 59),
+        new Time(12, 50, 0),
+        new Time(13, 29, 59),
         "study"
     ),
     new TimerItem(
         "5교시 쉬는시간",
-        new Time(14, 0, 0),
-        new Time(14, 4, 59),
+        new Time(13, 30, 0),
+        new Time(13, 39, 59),
         "not study"
     ),
     new TimerItem(
         "6교시",
-        new Time(14, 5, 0),
-        new Time(14, 54, 59),
+        new Time(13, 40, 0),
+        new Time(14, 19, 59),
         "study"
     ),
     new TimerItem(
         "6교시 쉬는시간",
-        new Time(14, 55, 0),
-        new Time(14, 59, 59),
+        new Time(14, 20, 0),
+        new Time(14, 29, 59),
         "not study"
     ),
     new TimerItem(
         "7교시",
-        new Time(15, 0, 0),
-        new Time(15, 49, 59),
+        new Time(14, 30, 0),
+        new Time(15, 9, 59),
         "study"
     ),
     new TimerItem(
         "방과후",
-        new Time(15, 50, 0),
+        new Time(15, 10, 0),
         new Time(23, 59, 59),
         "not study"
     ),
