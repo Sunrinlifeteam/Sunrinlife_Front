554e9b4225eda9fba40693cde06274ea4e14cc66
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2022-04-01 16:56:42 +0900

    remove: requireAuth middleware

--- src/router/router.js
+++ src/router/router.js
@@ -1,22 +1,14 @@
 import { createWebHistory, createRouter } from "vue-router"
-import { getUserData } from "./../api.js"
 
 import LoginPage from "./../views/LoginPage.vue"
 import Register from "./../views/RegisterPage.vue"
 import MainPage from "./../views/MainPage.vue"
-import ProfilePage from './../views/profilePage.vue';
+import ProfilePage from './../views/ProfilePage.vue';
 import ClubPage from "./../views/ClubPage.vue"
 import NoticePage from "./../views/NoticePage.vue"
 import NoticeDetailPage from "./../views/NoticeDetailPage.vue"
 
 import EasterEggPage from "./../views/EasterEggPage.vue"
-
-const requireAuth = () => (from, to, next) => {
-    if (getUserData() == null){
-        return next('/login')
-    }
-    return next()
-}
 
 const routes = [
     {
@@ -38,31 +30,26 @@ const routes = [
         path : "/",
         name: "main",
         component : MainPage,
-        beforeEnter: requireAuth(),
     },
     {
         path : "/notice",
         name: "notice",
         component : NoticePage,
-        beforeEnter: requireAuth(),
     },
     {
         path : "/notice/:noticeId",
         name: "noticeId",
         component : NoticeDetailPage,
-        beforeEnter: requireAuth(),
     },
     {
         path : "/profile",
         name: "profile",
         component : ProfilePage,
-        beforeEnter: requireAuth(),
     },
     {
         path : "/club",
         name: "club",
         component : ClubPage,
-        beforeEnter: requireAuth(),
         props: true
     },
 
@@ -70,7 +57,6 @@ const routes = [
         path : "/i_love_sunrin",
         name : "easterEgg",
         component : EasterEggPage,
-        beforeEnter: requireAuth(),
     }
 ]
 
