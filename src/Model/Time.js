3c427182b04b0629eaba6ca1cff05a9dcc94961b
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2022-06-05 01:23:27 +0900

    edit: add semicolon

--- src/Model/Time.js
+++ src/Model/Time.js
@@ -1,47 +1,47 @@
 export default class Time{
-    constructor(hour = -1, minute = -1, second = -1){
-        if(hour == -1 && minute == -1 && second == -1){
-            const currentTime = new Date()
+	constructor(hour = -1, minute = -1, second = -1){
+		if(hour == -1 && minute == -1 && second == -1){
+			const currentTime = new Date();
 
-            this.hour = currentTime.getHours()
-            this.minute = currentTime.getMinutes()
-            this.second = currentTime.getSeconds()
+			this.hour = currentTime.getHours();
+			this.minute = currentTime.getMinutes();
+			this.second = currentTime.getSeconds();
 
-            return
-        }
+			return;
+		}
 
-        this.hour = hour
-        this.minute = minute
-        this.second = second
-    }
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+	}
 
-    get allSecond(){
-        return this.hour * 60 * 60 + this.minute * 60 + this.second
-    }
+	get allSecond(){
+		return this.hour * 60 * 60 + this.minute * 60 + this.second;
+	}
 
-    get diffTimeForNow(){
-        return new Time().diffTime(this)
-    }
+	get diffTimeForNow(){
+		return new Time().diffTime(this);
+	}
 
-    diffTime(targetTime){
-        if(targetTime.second < this.second){
-            targetTime.second += 60
-            targetTime.minute--
-        }
-        const diffSecond = targetTime.second - this.second
+	diffTime(targetTime){
+		if(targetTime.second < this.second){
+			targetTime.second += 60;
+			targetTime.minute--;
+		}
+		const diffSecond = targetTime.second - this.second;
 
-        if(targetTime.minute < this.minute){
-            targetTime.minute += 60
-            targetTime.hour--
-        }
-        const diffMinute = targetTime.minute - this.minute
+		if(targetTime.minute < this.minute){
+			targetTime.minute += 60;
+			targetTime.hour--;
+		}
+		const diffMinute = targetTime.minute - this.minute;
 
         
-        if(targetTime.hour < this.hour)
-            targetTime.hour += 24
-        const diffhour = targetTime.hour - this.hour
+		if(targetTime.hour < this.hour)
+			targetTime.hour += 24;
+		const diffhour = targetTime.hour - this.hour;
 
 
-        return new Time(diffhour, diffMinute, diffSecond)
-    }
+		return new Time(diffhour, diffMinute, diffSecond);
+	}
 }
